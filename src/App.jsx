cde3a1d4a367e09540692d7c7cac7efb125bb22b
bakytbekov1ch <kalmamatovelibek32@gmail.com>
2024-08-23 15:45:01 +0600

    deleteAbout

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import HomePage from "./pages/HomePage";
-import About from "./components/About/About";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
-          <Route path="/about" element={<About />} />
         </Route>
       </Routes>
     </div>
