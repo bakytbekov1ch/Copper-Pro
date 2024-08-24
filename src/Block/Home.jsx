4b6570d3e59d908f6df29601a6ded7c135ba09e8
Tolobek <tsharsheev002@gmail.com>
2024-08-24 23:32:38 +0600

    change home page

--- src/Block/Home.jsx
+++ src/Block/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Testimonial from '../components/Testimonial/Testimonial'
+import Service from "../components/Service/Service"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+    </div>
   )
 }
 
